569ec82db2e4c6807c901527b703d8e852a441fd
Gustavo M. Scot <123573482+GustavoScot@users.noreply.github.com>
2025-04-02 11:07:59 -0300

    Add files via upload

---
+++ produto.py
@@ -0,0 +1,22 @@
+class Produto: 
+    def __init__(self, nome: str, preco: float, quantidade: int):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade = quantidade
+
+    def pegar_nome(self):
+        return self.nome
+        
+    def pegar_preco(self):
+        return self.preco
+        
+    def pegar_quantidade(self):
+        return self.quantidade
+
+    def todas_infos(self):
+        return f'Nome:{self.nome}\nPreço:{self.preco}\nQuantidade: {self.quantidade}'
+    
+produto1 = Produto('gustavo', 20.5, 10)
+
+print(produto1.todas_infos())
+
